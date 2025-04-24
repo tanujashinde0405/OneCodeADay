e93422b516a9a883e50d68516b4e2c6a802cfee7
Tanuja Shinde <tshinde@parallelwireless.com>
2025-04-24 15:15:59 +0000

    array basic problems

---
+++ arrays/last_index_of_one.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+// Given a string s consisting of only '0's and '1's,  find the last index of the '1' present.
+
+// Note: If '1' is not present, return "-1"
+
+// Examples:
+
+// Input: s = 00001
+// Output: 4
+// Explanation: Last index of  1 in given string is 4.
+// Time Complexity : O(n)
+// Space Complexity: O(1)
+int lastIndex(string s) {
+    int len = s.size();
+    for(int i=len-1;i>=0;i--){
+        if(s[i]=='1')
+            return i;
+    }
+    return -1;
+}
