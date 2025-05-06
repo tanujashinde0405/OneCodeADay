7102c8cbddadf16a86ea259ef05d49d28fc26593
Tanuja Shinde <tshinde@parallelwireless.com>
2025-05-06 03:48:26 +0000

    array problems

--- arrays/firstandlastx.cpp
+++ arrays/firstandlastx.cpp
@@ -27,3 +27,38 @@ vector<int> find(vector<int>& arr, int x) {
     ans.push_back(last);
     return ans;
 }
+
+// another approach
+// O(logn), O(1)
+int search(vector<int>& arr, int x, bool isfirst){
+    int n = arr.size();
+    int index = -1;
+    
+    int low = 0;
+    int high = n-1;
+    
+    while(low<= high){
+        int mid = (low+high)/2;
+        
+        if(arr[mid]==x){
+            index = mid;
+            
+            if(isfirst)
+                high = mid-1;
+            else
+                low =mid+1;
+        }
+        else if(x<arr[mid])
+            high = mid-1;
+        else
+            low = mid+1;
+    }
+    return index;
+}
+vector<int> find(vector<int>& arr, int x) {
+    int first = search(arr, x, true);
+    int last = search(arr, x, false);
+    
+    vector<int> ans = {first,last};
+    return ans;
+}
