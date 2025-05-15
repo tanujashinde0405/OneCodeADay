2766ce9bcee193bc3570fbbcfee2f8a66453d697
Tanuja Shinde <tshinde@parallelwireless.com>
2025-05-15 13:09:21 +0000

    string problems

---
+++ arrays/remove_element.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// Given an integer array nums and an integer val, remove all occurrences of val in nums in-place. The order of the elements may be changed. Then return the number of elements in nums which are not equal to val.
+
+// Example 1:
+
+// Input: nums = [3,2,2,3], val = 3
+// Output: 2, nums = [2,2,_,_]
+// Explanation: Your function should return k = 2, with the first two elements of nums being 2.
+// It does not matter what you leave beyond the returned k (hence they are underscores).
+
+int removeElement(vector<int>& nums, int val) {
+    int index = 0;
+    for(int i = 0; i< nums.size(); i++){
+        if(nums[i] != val){
+            nums[index] = nums[i];
+            index++;
+        }
+    }
+    return index;
+}
