7bb9dc024dfd020cf3ccb4c99d0026fa2b7a701e
Tanuja Shinde <tshinde@parallelwireless.com>
2025-05-20 04:35:29 +0000

    matrix problems

---
+++ matrix/rotate_matrix_anticlosewise.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+// Given a square matrix mat[][] of size n x n. The task is to rotate it by 90 degrees in an anti-clockwise direction without using any extra space. 
+
+// Examples:
+
+// Input: mat[][] = [[1, 2, 3],
+//                 [4, 5, 6]
+//                 [7, 8, 9]]
+// Output: Rotated Matrix:
+// [3, 6, 9]
+// [2, 5, 8]
+// [1, 4, 7]
+
+
+void rotateby90(vector<vector<int>>& mat) {
+    int n = mat.size();
+    
+    for(int i=0;i<n;i++){
+        reverse(mat[i].begin(), mat[i].end());
+    }
+    
+    for(int i=0;i<n;i++){
+        for(int j=i+1;j<n;j++){
+            swap(mat[i][j], mat[j][i]);
+        }
+    }
+}
