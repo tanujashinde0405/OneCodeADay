b5ec8363a4d07fcfc9f81a1a1f719d9d65c2239a
Tanuja Shinde <tshinde@parallelwireless.com>
2025-05-20 04:31:17 +0000

    array problems

--- arrays/rotate_matrix_clockwise.cpp
+++ arrays/rotate_matrix_clockwise.cpp
@@ -11,7 +11,8 @@ using namespace std;
 // 7 4 1 
 // 8 5 2
 // 9 6 3
-
+// time complexity: O(n^2)
+// space complexity: O(1)
 
 void rotate(vector<vector<int> >& matrix) {
     int n = matrix.size();
@@ -24,4 +25,17 @@ void rotate(vector<vector<int> >& matrix) {
     for(int i=0;i<n;i++){
         reverse(matrix[i].begin(), matrix[i].end());
     }
+}
+
+int main() {
+    vector<vector<int>> matrix = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+    rotate(matrix);
+    cout << "Rotated matrix is: " << endl;
+    for(int i=0;i<matrix.size();i++){
+        for(int j=0;j<matrix[i].size();j++){
+            cout << matrix[i][j] << " ";
+        }
+        cout << endl;
+    }
+    return 0;
 }
