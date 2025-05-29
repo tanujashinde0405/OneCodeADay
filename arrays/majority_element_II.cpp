1907ebc0663995814a462eae50391bdfcd0432c9
Tanuja Shinde <tshinde@parallelwireless.com>
2025-05-29 14:42:06 +0000

    majority element II

---
+++ arrays/majority_element_II.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+// You are given an array of integer arr[] where each number represents a vote to a candidate. Return the candidates that have votes greater than one-third of the total votes, If there's not a majority vote, return an empty array. 
+
+// Note: The answer should be returned in an increasing format.
+
+// Examples:
+
+// Input: arr[] = [2, 1, 5, 5, 5, 5, 6, 6, 6, 6, 6]
+// Output: [5, 6]
+// Explanation: 5 and 6 occur more n/3 times.
