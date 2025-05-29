1ca4661284c058242fea2668be149c849e235151
Tanuja Shinde <tshinde@parallelwireless.com>
2025-05-29 14:42:34 +0000

    majority element II

--- arrays/majority_element_II.cpp
+++ arrays/majority_element_II.cpp
@@ -11,3 +11,45 @@ using namespace std;
 // Input: arr[] = [2, 1, 5, 5, 5, 5, 6, 6, 6, 6, 6]
 // Output: [5, 6]
 // Explanation: 5 and 6 occur more n/3 times.
+
+vector<int> findMajority(vector<int>& nums) {
+    int n = nums.size();
+    int candidate1 = -1, candidate2 = -1;
+    int cnt1 = 0, cnt2 = 0;
+    vector<int> res;
+    for(int num : nums){
+        if (candidate1 == num){
+            cnt1++;
+        }
+        else if (candidate2 == num){
+            cnt2++;
+        }
+        else if(cnt1 == 0){
+            candidate1 = num;
+            cnt1++;
+        }
+        else if(cnt2 == 0){
+            candidate2 = num;
+            cnt2++;
+        }
+        else{
+            cnt1--;
+            cnt2--;
+        }
+    }
+    cnt1 = 0;
+    cnt2 =0;
+    for(int num : nums){
+        if(candidate1 == num)
+            cnt1++;
+        if(candidate2 == num)
+            cnt2++;
+    }
+    if(cnt1 > n/3)
+        res.push_back(candidate1);
+    if(cnt2 > n/3 && candidate1 != candidate2)
+        res.push_back(candidate2);
+    if(res.size() == 2 && res[0] > res[1])
+        swap(res[0], res[1]);
+    return res;
+}
