ad67fbfb17743b2c247ffaaab6f79ea83f2a3473
Tanuja Shinde <tshinde@parallelwireless.com>
2025-05-30 08:22:59 +0000

    pattern problems

--- patterns/pattern9.cpp
+++ patterns/pattern9.cpp
@@ -2,6 +2,14 @@
 #include <bits/stdc++.h>
 using namespace std; 
 
+// *
+// **
+// ***
+// ****
+// ***
+// **
+// *
+
 void printPattern(int n) {
     for(int i=1;i<=2*n-1;i++){
         int stars = i;
