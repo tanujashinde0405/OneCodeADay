06590fe0c42f6d1bd0004163b77fa7f0a958a5bc
Tanuja Shinde <tshinde@parallelwireless.com>
2025-05-30 06:38:14 +0000

    pattern problems

---
+++ patterns/pattern1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+void printPattern(int n) {
+    for(int i=0;i<n;i++) {
+        for(int j=0;j<n;j++) {
+            cout<<"* ";
+        }
+        cout << endl;
+    }
+}
+
+int main(){
+    printPattern(4);
+}
