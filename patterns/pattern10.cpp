12e6645920765e2697abbc400b354fdaa4ba8fe4
Tanuja Shinde <tshinde@parallelwireless.com>
2025-06-03 08:24:37 +0000

    patern 10

---
+++ patterns/pattern10.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// output
+// 1
+// 01
+// 101
+// 0101
+// 10101
+
+void pattern11(int N)
+{
+      int start =1;
+      for(int i=0;i<N;i++){
+          if(i%2 ==0) start = 1;
+          else start = 0;
+          for(int j=0;j<=i;j++){
+              cout<<start;
+              start = 1-start;
+          }
+        cout<<endl;
+      }
+}
+
+int main()
+{
+    int N = 5;
+    pattern11(N);
+
+    return 0;
+}
