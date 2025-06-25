66fc4903142cdbf79e51b4c16f6f3c952e10577e
Tanuja Shinde <tshinde@parallelwireless.com>
2025-06-25 14:29:01 +0000

    pattern problems

--- patterns/pattern5.cpp
+++ patterns/pattern5.cpp
@@ -2,6 +2,10 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+// * * * * 
+// * * * 
+// * * 
+// * 
 void printPattern(int n) {
     for(int i=1;i<=n;i++){
         for(int j=0;j<n-i+1;j++){
--- patterns/pattern6.cpp
+++ patterns/pattern6.cpp
@@ -2,6 +2,10 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+// 1 2 3 4 
+// 1 2 3 
+// 1 2 
+// 1 
 void printPattern(int n) {
     for(int i=1;i<=n;i++){
         for(int j=0;j<n-i+1;j++){
